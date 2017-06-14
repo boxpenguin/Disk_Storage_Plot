9b5162accacf701aa456ca0ff8c22652234a316b
UEHARA <juehara@gs6151-isfsadev.ndc.nasa.gov>
2017-06-14 09:44:14 -0400

    first

---
+++ Plot_Disk_Storage.py
@@ -0,0 +1,171 @@
+##!/usr/bin/python3.5
+#!/Applications/anaconda3/anaconda/bin/python3.6
+import csv, os, re
+"""
+Read section:
+Load archival data
+Load all newer data
+"""
+# path = "/var/www/html/_admin/Disk_Storage/" # ubuntu dev
+path = "/Users/juehara/opt/clara-disk/Disk_Storage/" # mac dev
+archive_file = "archive/archived_Disk_Storage-1.csv"
+transpose_file = "archive/diskstorage-transpose-1.csv"
+
+date_array = []
+tv_array = []
+movies_array = []
+anime_array = []
+special_array = []
+user_array = []
+plex_array = []
+music_array = []
+backups_array = []
+media_array = []
+danish_array = []
+eclair_array = []
+granola_array = []
+mochi_array = []
+mousse_array = []
+# Archive Data
+with open(path+transpose_file) as archive_csv:
+    read = csv.reader(archive_csv, delimiter=',')
+    for row in read:
+        if row:
+            date = row[0]
+            tv = row[1]
+            movies = row[2]
+            anime = row[3]
+            special = row[4]
+            user = row[5]
+            plex = row[6]
+            music = row[7]
+            backups = row[8]
+            media = row[9]
+            danish = row[10]
+            eclair = row[11]
+            granola = row[12]
+            mochi = row[13]
+            mousse = row[14]
+            date_array.append(date)
+            tv_array.append(tv)
+            movies_array.append(movies)
+            anime_array.append(anime)
+            special_array.append(special)
+            user_array.append(user)
+            plex_array.append(plex)
+            music_array.append(music)
+            backups_array.append(backups)
+            media_array.append(media)
+            danish_array.append(danish)
+            eclair_array.append(eclair)
+            granola_array.append(granola)
+            mochi_array.append(mochi)
+            mousse_array.append(mousse)
+
+del date_array[0], tv_array[0], movies_array[0], anime_array[0]
+del special_array[0], user_array[0], plex_array[0], music_array[0]
+del backups_array[0], media_array[0], danish_array[0], eclair_array[0]
+del granola_array[0], mochi_array[0], mousse_array[0]
+
+# New Data
+regex_date = r"201\d-\d\d-\d\d"
+regex_value = r"\d+"
+newpath = path + "new/"
+
+for path, subdirs, files in os.walk(newpath):
+    for name in sorted(files, key=lambda name: os.path.getmtime(os.path.join(newpath, name))):
+        datafile = open(newpath + name, "r")
+        match = re.search(regex_date,name)
+        date_array.append(match.group(0))
+        for line in datafile:
+            if "Anime Total" in line:
+                match = re.search(regex_value, line)
+                anime_array.append(match.group(0))
+            elif "TV Shows Total" in line:
+                match = re.search(regex_value, line)
+                tv_array.append(match.group(0))
+            elif "Movies Total" in line:
+                match = re.search(regex_value, line)
+                movies_array.append(match.group(0))
+            elif "Specials Total" in line:
+                match = re.search(regex_value, line)
+                special_array.append(match.group(0))
+            elif "Users Total" in line:
+                match = re.search(regex_value, line)
+                user_array.append(match.group(0))
+            elif "Plex Database Total" in line:
+                match = re.search(regex_value, line)
+                plex_array.append(match.group(0))
+            elif "Music Total" in line:
+                match = re.search(regex_value, line)
+                music_array.append(match.group(0))
+            elif "Backups Total" in line:
+                match = re.search(regex_value, line)
+                backups_array.append(match.group(0))
+            elif "/media/ " in line:
+                match = re.search(regex_value, line)
+                media_array.append(match.group(0))
+            elif "/media/DANISH " in line:
+                match = re.search(regex_value, line)
+                danish_array.append(match.group(0))
+            elif "/media/ECLAIR " in line:
+                match = re.search(regex_value, line)
+                eclair_array.append(match.group(0))
+            elif "/media/GRANOLA " in line:
+                match = re.search(regex_value, line)
+                granola_array.append(match.group(0))
+            elif "/media/MOCHI " in line:
+                match = re.search(regex_value, line)
+                mochi_array.append(match.group(0))
+            elif "/media/MOUSSE " in line:
+                match = re.search(regex_value, line)
+                mousse_array.append(match.group(0))
+
+        # read = csv.reader(datafile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        # for row in read:
+        #     print (row)
+        #     if row:
+        #         tv = row[1]
+        #         movies = row[2]
+        #         anime = row[3]
+        #         special = row[4]
+        #         user = row[5]
+        #         plex = row[6]
+        #         music = row[7]
+        #         backups = row[8]
+        #         media = row[9]
+        #         danish = row[10]
+        #         eclair = row[11]
+        #         granola = row[12]
+        #         mochi = row[13]
+        #         mousse = row[14]
+        #         tv_array.append(tv)
+        #         movies_array.append(movies)
+        #         anime_array.append(anime)
+        #         special_array.append(special)
+        #         user_array.append(user)
+        #         plex_array.append(plex)
+        #         music_array.append(music)
+        #         backups_array.append(backups)
+        #         media_array.append(media)
+        #         danish_array.append(danish)
+        #         eclair_array.append(eclair)
+        #         granola_array.append(granola)
+        #         mochi_array.append(mochi)
+        #         mousse_array.append(mousse)
+
+print (date_array)
+print (tv_array)
+print (movies_array)
+print (anime_array)
+print (special_array)
+print (user_array)
+print (plex_array)
+print (music_array)
+print (backups_array)
+print (media_array)
+print (danish_array)
+print (eclair_array)
+print (granola_array)
+print (mochi_array)
+print (mousse_array)
---
+++ Write_Disk_Storage.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3.5
+import csv, time, subprocess, re
+date = time.strftime("%Y-%m-%d")
+file = ("Disk_storage-{0}.csv" .format(date))
+
+tv_shows_loc = ["/media/DANISH/Media/TV\ Shows"]
+tv_shows_val = [""]
+movies_loc = ["/media/ECLAIR/Movies"]
+movies_val = [""]
+anime_loc = ["/media/MOCHI/Anime", "/media/ECLAIR/Anime"]
+anime_val = [""]
+specials_loc = ["/media/DANISH/Media/Special\ Libraries"]
+specials_val = [""]
+user_loc = ["/home/clara", "/media/MOUSSE/home"]
+user_val = [""]
+plex_loc = ["/var/lib/plexmediaserver"]
+plex_val = [""]
+music_loc = ["/media/DANISH/Media/Music"]
+music_val = [""]
+backup_loc = ["/media/GRANOLA/Backups-Muffin"]
+backup_val = [""]
+media_loc = ["/media "]
+media_val = [""]
+danish_loc = ["/media/DANISH "]
+danish_val = [""]
+# donut_loc = ["/Volumes/DATA/media/DONUT"]
+# donut_val = [""]
+eclair_loc = ["/media/ECLAIR "]
+eclair_val = [""]
+granola_loc = ["/media/GRANOLA "]
+granola_val = [""]
+mochi_loc = ["/media/MOCHI "]
+mochi_val = [""]
+mousse_loc = ["/media/MOUSSE "]
+mousse_val = [""]
+# master_loc = [tv_shows_loc, movies_loc, anime_loc, specials_loc, user_loc,
+#     plex_loc, music_loc, backup_loc, media_loc, danish_loc, donut_loc,
+#     eclair_loc, granola_loc, mochi_loc, mousse_loc]
+
+command_mac = "du -shm "
+command_linux = "du -shBM "
+command = command_linux
+
+def fillarray(array_loc, array_val):
+    regex = r"^\d+"
+    for items in array_loc:
+        b_output = subprocess.check_output(command + items, shell=True)
+        output = b_output.decode("utf-8")
+        match = re.match(regex, output)
+        array_val.append(int(match.group(0)))
+    del array_val[0]
+    print (array_val)
+    print (array_loc)
+
+fillarray(tv_shows_loc,tv_shows_val)
+fillarray(movies_loc,movies_val)
+fillarray(anime_loc,anime_val)
+fillarray(specials_loc,specials_val)
+fillarray(user_loc,user_val)
+fillarray(plex_loc,plex_val)
+fillarray(music_loc,music_val)
+fillarray(backup_loc,backup_val)
+fillarray(media_loc,media_val)
+fillarray(danish_loc,danish_val)
+#fillarray(donut_loc,donut_val)
+fillarray(eclair_loc,eclair_val)
+fillarray(granola_loc,granola_val)
+fillarray(mochi_loc,mochi_val)
+fillarray(mousse_loc,mousse_val)
+
+def writearray(write, array_loc, array_val, name, multi=True):
+    i = 0
+    for items in array_loc:
+        write.writerow([array_val[i], array_loc[i]])
+        i += 1
+    if multi == True:
+        write.writerow([sum(array_val), '{0} Total' .format(name)])
+
+"""
+Writing section
+"""
+with open(file, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writearray(writer, anime_loc, anime_val, "Anime")
+        writearray(writer, tv_shows_loc,tv_shows_val, "TV Shows")
+        writearray(writer, movies_loc,movies_val, "Movies")
+        writearray(writer, specials_loc,specials_val, "Specials")
+        writearray(writer, user_loc,user_val, "Users")
+        writearray(writer, plex_loc,plex_val, "Plex Database")
+        writearray(writer, music_loc,music_val, "Music")
+        writearray(writer, backup_loc,backup_val, "Backups")
+        writearray(writer, media_loc,media_val, "Storage", False)
+        writearray(writer, danish_loc,danish_val, "DANISH", False)
+        # writearray(writer, donut_loc,donut_val, "DONUT")
+        writearray(writer, eclair_loc,eclair_val, "ECLAIR", False)
+        writearray(writer, granola_loc,granola_val, "GRANOLA", False)
+        writearray(writer, mochi_loc,mochi_val, "MOCHI", False)
+        writearray(writer, mousse_loc,mousse_val, "MOUSSE", False)
